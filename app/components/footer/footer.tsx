b4c96cf5d123eb896c917746ca36a2223a75f6c2
Alihamxa01 <hamza.nadeem.0106@gmail.com>
2024-11-10 17:26:01 +0500

    Description of the changes you made

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,4 +1,12 @@
 
+
+
+
+
+
+
+
+
 import React from 'react';
 import { FaLinkedin } from 'react-icons/fa';
 
@@ -12,32 +20,23 @@ export default function Footer() {
         Developing the future, one website at a time.
       </p>
 
-       <section id='overview' className="text-left border-t-2">
+      <section id="overview" className="text-left border-t-2">
         <h2 className="text-4xl font-black mb-4 pl-5 pb-2">
           <span className="text-9xl text-green-400">A</span>BOUT <span className="text-white">US</span>
         </h2>
-        <p className="text-xl font-medium  mx-auto border-l-4 border-green-400 p-3">
-          Welcome to my <span className='text-1xl text-green-400 font-semibold'>PORTFOLIO</span> website! This platform showcases my skills and projects as a developer.<br/> Here, you'll find a collection of my work, including web development projects and design creations, <br/>highlighting my journey and expertise in the field.
+        <p className="text-xl font-medium mx-auto border-l-4 border-green-400 p-3">
+          Welcome to my <span className="text-1xl text-green-400 font-semibold">PORTFOLIO</span> website! This platform showcases my skills and projects as a developer.<br /> Here, you&apos;ll find a collection of my work, including web development projects and design creations,<br />highlighting my journey and expertise in the field.
         </p>
-       
-
-    
       </section>
 
-<section>
-  <h1 className='text-green-400 text-3xl font-black   text-right mr-10 mt-36'>FOR CONTACT</h1>
-  <p className='text-lg text-right m-7'>  If you have any questions, feel free to contact me.<br/> I'm always open to work and new ideas. You can email me at: 
-  </p> 
-  
-  
+      <section>
+        <h1 className="text-green-400 text-3xl font-black text-right mr-10 mt-36">FOR CONTACT</h1>
+        <p className="text-lg text-right m-7">If you have any questions, feel free to contact me.<br /> I&apos;m always open to work and new ideas. You can email me at:</p>
 
-  <h1 className='pr-7 text-lg mt-2  font-thin text-end'>
-  hamza.nadeem.0106@gmail.com 
-  </h1>
-  
-</section> 
-
-
+        <h1 className="pr-7 text-lg mt-2 font-thin text-end">
+          hamza.nadeem.0106@gmail.com
+        </h1>
+      </section>
 
       {/* Footer Bottom */}
       <div className="mt-8 text-center text-sm text-gray-500 border-t border-gray-700 pt-4 border-dashed">
@@ -46,33 +45,18 @@ export default function Footer() {
         {/* LinkedIn Section with separate "Connect with me" text */}
         <div className="flex justify-start mt-2">
           <span className="text-white font-medium mr-2">Connect with me</span>
-          <a href='https://www.linkedin.com/in/ali-hamza-nadeem-479b42292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target="_blank" className="hover:text-blue-500">
-            <FaLinkedin className='text-blue-700 size-8 hover:text-blue-500 duration-75 hover:size-10' />
+          <a 
+            href="https://www.linkedin.com/in/ali-hamza-nadeem-479b42292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="hover:text-blue-500"
+          >
+            <FaLinkedin className="text-blue-700 size-8 hover:text-blue-500 duration-75 hover:size-10" />
           </a>
         </div>
       </div>
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
       {/* Main Title Section */}
       <section className="flex flex-col  justify-center h-screen bg-green-500 text-center  duration-300 ease-in-out hover:bg-green-600">
         <h1  className="text-8xl font-extrabold  drop-shadow-xl animate-bounce">
-          HEY, I'M ALI HAMZA
+          HEY, IAM ALI HAMZA
         </h1>
         <span className="text-4xl mt-2">FRONT-END DEVELOPER</span>
       
@@ -26,15 +26,15 @@ export default function Page() {
 
 
       {/* About Me Section */}
-      <section id='about me'  className=" bg-teal-900   text-center border-b-2 border-t-2 h-96" >
+      <section id="about me"  className=" bg-teal-900   text-center border-b-2 border-t-2 h-96" >
         <h2 className="text-6xl font-black mb-4">
           <span className="text-9xl text-green-400">A</span>BOUT <span className="text-white">ME</span>
         </h2>
          
         <p className="text-xl font-medium leading-relaxed mx-auto border-l-4 border-green-400 pl-4 p-7">
-          I'm <span className='text-1xl text-green-400 font-semibold'>Ali Hamza</span>, a new developer who knows HTML, CSS, TypeScript, 
-          Nextjs and Tailwind css . I'm still learning and getting better at building websites. Right now, I'm enrolled in a course at Government
-           House, where I'm learning more skills to improve my work. I enjoy working on creative projects and love exploring new ways to make my
+          I am <span className="text-1xl text-green-400 font-semibold">Ali Hamza</span>, a new developer who knows HTML, CSS, TypeScript, 
+          Nextjs and Tailwind css . Iam still learning and getting better at building websites. Right now, Iam enrolled in a course at Government
+           House, where I&apos;m learning more skills to improve my work. I enjoy working on creative projects and love exploring new ways to make my
             websites better and more responsive.
         </p>
       </section>
@@ -43,7 +43,7 @@ export default function Page() {
       <div className="border-t-4 border-green-400 my-10  "></div>
 
       {/* privous work Section */}
-      <section id='project' className="px-6 py-10 bg-teal-900 rounded-lg shadow-2xl text-center border-b-2 border-t-2">
+      <section id="project" className="px-6 py-10 bg-teal-900 rounded-lg shadow-2xl text-center border-b-2 border-t-2">
   <h2 className="text-6xl font-black mb-4">
     <span className="text-9xl text-green-400">MY</span>PROJECTS <span className="text-white"></span>
   </h2>
@@ -53,39 +53,39 @@ export default function Page() {
 
     {/* RESUME BUILDER (Left) */}
     <div className="w-full lg:w-1/2 bg-transparent text-center pt-4 px-6">
-      <h1 className='text-4xl font-mono font-bold items-center text-green-500'>
-        <span className='text-8xl'>D</span>YNAMIC RESUME BUILDER
+      <h1 className="text-4xl font-mono font-bold items-center text-green-500">
+        <span className="text-8xl">D</span>YNAMIC RESUME BUILDER
       </h1>
       {/* image1 */}
       <div className="max-w-xl hover:scale-105 hover:shadow-green-400 shadow-lg hover:bg-teal-800 duration-200 pt-3 mt-5 rounded-lg">
         <a>
-          <Image src='/img2.jpg' alt="photo" height='300' width='300' className='mx-auto mt-5 shadow-lg bg-transparent hover:bg-teal-800 hover:shadow-lg hover:shadow-green-400 duration-200 rounded-lg' />
+          <Image src="/img2.jpg" alt="photo" height="300" width="300" className="mx-auto mt-5 shadow-lg bg-transparent hover:bg-teal-800 hover:shadow-lg hover:shadow-green-400 duration-200 rounded-lg" />
         </a>
         {/* para 1*/}
         <p className="text-xl font-medium mt-4 text-white pb-6 px-6">
           This is a dynamic resume builder website where you can fill in your information, and a resume will be generated for you. You can also edit the resume later as needed.<br /><br />
-          Here is the link: <a href="https://sparkling-panda-2ec022.netlify.app/" target="_blank" className='text-blue-200 hover:text-green-400'>you can try this!</a>
+          Here is the link: <a href="https://sparkling-panda-2ec022.netlify.app/" target="_blank" className="text-blue-200 hover:text-green-400">you can try this!</a>
         </p>
       </div>
     </div>
 
     {/* MUSIC PLAYER (Right) */}
     <div className="w-full lg:w-1/2 bg-transparent text-center pt-4 px-6">
-      <h1 className='text-4xl font-mono font-bold text-center text-green-500'>
-        <span className='text-8xl'>MR</span>FOODIE
+      <h1 className="text-4xl font-mono font-bold text-center text-green-500">
+        <span className="text-8xl">MR</span>FOODIE
       </h1>
       {/* img2 */}
       <div className="max-w-xl hover:scale-105 hover:shadow-green-400 shadow-lg hover:bg-teal-800 duration-200 pt-3 mt-5 rounded-lg">
         <a>
-          <Image src='/img4.jpg' alt="photo" height='300' width='300' className='mx-auto mt-5 shadow-lg bg-transparent hover:bg-teal-800 hover:shadow-lg hover:shadow-green-400 duration-200 rounded-lg' />
+          <Image src="/img4.jpg" alt="photo" height="300" width="300" className="mx-auto mt-5 shadow-lg bg-transparent hover:bg-teal-800 hover:shadow-lg hover:shadow-green-400 duration-200 rounded-lg" />
         </a>
         {/* para 2 */}
         <p className="text-xl font-medium pb-3 mt-4 text-white px-6">
-        I created a website named "Mr. Foodie" to showcase my skills, featuring a menu bar, header, footer, and a hero section with food images. It also includes a fast food menu.
+        I created a website named Mr. Foodie to showcase my skills, featuring a menu bar, header, footer, and a hero section with food images. It also includes a fast food menu.
 
 
 </p>
-        <br /> Here is the link: <a href="https://foodie-com-website2.vercel.app/" target="_blank" className='text-blue-200 hover:text-green-400 hover:text-lg'>you can try this!</a>
+        <br /> Here is the link: <a href="https://foodie-com-website2.vercel.app/" target="_blank" className="text-blue-200 hover:text-green-400 hover:text-l">you can try this!</a>
       </div>
     </div>
 
@@ -96,7 +96,7 @@ export default function Page() {
 {/* service */}
 
 
-<section id='service'  className=" bg-teal-900   text-center border-b-2 border-t-2 h-96" >
+<section id="service"  className=" bg-teal-900   text-center border-b-2 border-t-2 h-96" >
         <h2 className="text-6xl font-black mb-4">
           <span className="text-9xl text-green-400">S</span>ERVICE
         </h2>
