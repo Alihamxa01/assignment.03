340cd8e7847bdd7556a6501e64e9865f7d7edf48
ALI HAMZA NADEEM RANA <159229677+Alihamxa01@users.noreply.github.com>
2025-02-13 20:34:43 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Header from '../app/components/Header/Header';
+import Footer from '../app/components/Footer/Footer'
 import Image from 'next/image';
 
 export default function Page() {
